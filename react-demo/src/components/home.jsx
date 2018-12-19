1fc23330d91d424d4bdaf2c5cc4c3cbf188cfeb0
Sukumar Medidi <sukumarmedidi3691172@gmail.com>
2018-12-18 19:33:24 -0500

    added axios service call

--- react-demo/src/components/home.jsx
+++ react-demo/src/components/home.jsx
@@ -1,9 +1,53 @@
 import React, { Component } from "react";
+import axios from 'axios';
 class Home extends Component {
-  state = {};
-  render() {
-    return <h1>Home Page</h1>;
+  state = {
+    data : []
+  };
+
+  constructor(){
+    super();
   }
+
+  componentDidMount = () => {
+    let self = this;
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(function (response) {
+         self.setState({ 
+           data : response.data}
+           );
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    }
+    render() {
+      return (
+  <table class="table" style={{marginTop : 15}}>
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">S.No</th>
+      <th scope="col">Name</th>
+      <th scope="col">Email</th>
+      <th scope="col">phone</th>
+    </tr>
+  </thead>
+  <tbody>
+    {this.state.data.map(item => {
+     return <tr>
+      <th scope="row">{item.id}</th>
+      <td>{item.name}</td>
+      <td>{item.email}</td>
+      <td>{item.phone}</td>
+    </tr>
+    })}
+
+   
+  </tbody>
+</table>
+
+      );
+    }
 }
 
 export default Home;
